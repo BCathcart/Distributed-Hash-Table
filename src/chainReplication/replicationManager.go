a3ff5ee23d583fa032b4df74f9dc8c408a65cb03
tBroatch <thomas.broatch98@gmail.com>
2021-03-15 20:30:53 -0700

    More update predecessor code.

--- src/chainReplication/replicationManager.go
+++ src/chainReplication/replicationManager.go
@@ -6,6 +6,7 @@ import (
 	pb "github.com/CPEN-431-2021/dht-abcpen431/pb/protobuf"
 	kvstore "github.com/CPEN-431-2021/dht-abcpen431/src/kvStore"
 	"google.golang.org/protobuf/proto"
+	"github.com/CPEN-431-2021/dht-abcpen431/src/util"
 )
 
 // TODO(Brennan): should add callback to reqreply layer so that
@@ -24,9 +25,12 @@ type successorNode struct {
 
 // TODO: need reference to this nodes KV store?
 
+// The key field is redundant as our key is calculated by taking the crc32 of the IP/port string, however storing
+// simplifies the code.
 type predecessorNode struct {
 	keys       keyRange
 	addr       *net.Addr
+	key uint32
 	transfered bool
 	// TODO: add kvStore instance here?
 }
@@ -34,6 +38,7 @@ type predecessorNode struct {
 // 0 = first, 1 = second, 2 = third (not part of the chain but necessary to get lower bound)
 var predecessors [3]*predecessorNode
 var successor *successorNode
+var thisKey uint32
 
 var pendingSendingTransfers []*net.Addr
 var sendingTransfers []*net.Addr
@@ -53,49 +58,86 @@ func NewBootstrappingPredecessor(addr *net.Addr) {
 	// - simply call a function to drop all keys outside expected range
 }
 
+/*
+	Helper function for getting predecessor address.
+ */
+func getPredAddr(predIdx int) *net.Addr{
+	if predecessors[predIdx] == nil {
+		return nil
+	}
+	return predecessors[predIdx].addr
+}
+
+func getPredKey(addr *net.Addr) uint32{
+	if addr == nil {
+		return thisKey
+	}
+	return util.GetAddrKey(addr)
+}
+
+func updatePredecessors(firstPredAddr *net.Addr, secondPredAddr *net.Addr, thirdPredAddr *net.Addr){
+
+}
+
 // TODO: may need to update both predecessors at once
-func UpdatePredecessors(firstPredAddr *net.Addr, secondPredAddr *net.Addr, thirdPredAddr *net.Addr) {
+func checkPredecessors(newPredAddr1 *net.Addr, newPredAddr2 *net.Addr, newPredAddr3 *net.Addr) {
 	// If the new predecessor is not the previous predessor's predecessor,
 	// then start the Bootstrap transfer to the newly joined node
 
-	// Not sure if can compare stuff like this.
-	pred1Equal := firstPredAddr == predecessors[0].addr
-	pred2Equal := secondPredAddr == predecessors[1].addr
-	pred3Equal := thirdPredAddr == predecessors[2].addr
+	// Setup code keys based on addresses
+	oldPredAddr1, oldPredAddr2, oldPredAddr3 := getPredAddr(0), getPredAddr(1), getPredAddr(2)
+	oldPredKey1, oldPredKey2, oldPredKey3 := getPredKey(oldPredAddr1), getPredKey(oldPredAddr2), getPredKey(oldPredAddr3)
+	newPredKey1, newPredKey2, newPredKey3  := getPredKey(newPredAddr1), getPredKey(newPredAddr2), getPredKey(newPredAddr3)
+	pred1Equal := newPredKey1 == oldPredKey1
+	pred2Equal := newPredKey2 == oldPredKey2
+	pred3Equal := newPredKey3 == oldPredKey3
+
 	// If none of the previous three have changed, no need to update.
 	if pred1Equal && pred2Equal && pred3Equal {
 		return
 	}
-	// Will need to handle case by case basis - lots of different scenarios.
 
-	// Case 1: First and second predecessors stay the same, third predecessor is different
-	// Action: Drop appropriate keys
+	updatePredecessors(newPredAddr1, newPredAddr2, newPredAddr3)
+
+	// Will need to handle case by case basis - many different scenarios.
+	/*
+		Case 1: First and second predecessors stay the same (third is different).
+		This could mean either the third predecessor has failed, or a new node has joined
+		between the second and third predecessor.
+	 */
 	if pred1Equal && pred2Equal {
-		// callSweeper()
+		// New node has joined
+		if oldPredAddr3 == nil || util.BetweenKeys(newPredKey3, oldPredKey3, oldPredKey2){
+			// TODO: sweepCache(oldPredKey3, oldPredKey2)
+		} else{ // P3 failed. Will be receiving P3 keys from P1
+			pendingRcvingTransfers = append(pendingRcvingTransfers, newPredAddr1)
+		}
+	}else if {
+
 	}
+
+
 
 	// TODO: handle case second predecessor stays the same but its key range changes
 	// - drop keys we are no longer responsible for
 
 	// NOTE: this case handled by having membership layer NewBootstrappingPredecessor
-	// if predecessors[0].addr != secondPredAddr {
-	// 	startBootstrapTransfer(firstPredAddr)
+	// if predecessors[0].addr != newPredAddr2 {
+	// 	startBootstrapTransfer(newPredAddr1)
 
 	// 	// Will need to drop keys after the transfer is finished
 	// 	// - simply call a function to drop all keys outside expected range
 
 	// } else
-
-	// TODO(Brennan): do proper comparisons
-	if predecessors[0].addr == firstPredAddr && predecessors[1].addr != secondPredAddr {
+	if predecessors[0].addr == newPredAddr1 && predecessors[1].addr != newPredAddr2 {
 		// If it's a new member, expect to drop some keys
 
 		// If it's an existing member (second predecessor failed), expect to get transfered keys the second predecessor is responsible for
-		pendingRcvingTransfers = append(pendingRcvingTransfers, secondPredAddr)
+		pendingRcvingTransfers = append(pendingRcvingTransfers, newPredAddr2)
 
-	} else if predecessors[1].addr == firstPredAddr {
+	} else if predecessors[1].addr == newPredAddr1 {
 		// First predecessor has failed, expect to get transferred some keys the new second predecessor is responsible for
-		pendingRcvingTransfers = append(pendingRcvingTransfers, secondPredAddr)
+		pendingRcvingTransfers = append(pendingRcvingTransfers, newPredAddr2)
 
 		// Transfer new first predecessor's keys to successor
 		sendDataTransferReq(successor.addr)
