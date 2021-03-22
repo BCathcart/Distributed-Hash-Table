966b77a8ffb6fa76290525dc4bf796b68e7a40db
Brennan <brennancathcart@gmail.com>
2021-03-21 17:53:37 -0700

    Preventing from sending membership request to yourself

--- src/membership/membership.go
+++ src/membership/membership.go
@@ -66,12 +66,19 @@ func makeMembershipReq(otherMembers []*net.UDPAddr, thisIP string, thisPort int3
 		// Send request to random node (from list of nodes)
 		randIdx := rand.Intn(len(otherMembers))
 		randAddr := otherMembers[randIdx]
+		localAddrStr := util.CreateAddressString(thisIP, int(thisPort))
+
+		var randAddrTmp net.Addr = randAddr
+		if util.CreateAddressStringFromAddr(&randAddrTmp) == localAddrStr {
+			log.Println("WARN: cannot send membership request to yourself")
+			continue
+		}
+
+		log.Println("NOT THE LOCAL ADDRESS  ", util.CreateAddressStringFromAddr(&randAddrTmp), "  ", localAddrStr)
 
 		// Send heartbeat to the node to get gossip started of our existence started
-		var addr net.Addr = randAddr
-		gossipHeartbeat(&addr)
+		gossipHeartbeat(&randAddrTmp)
 
-		localAddrStr := util.CreateAddressString(thisIP, int(thisPort))
 		reqPayload := []byte(localAddrStr)
 		err := requestreply.SendMembershipRequest(reqPayload, randAddr.IP.String(), randAddr.Port)
 		if err != nil {
