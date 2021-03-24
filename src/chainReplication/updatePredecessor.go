812b5fd59441f8e9ac56fc35926c827414c11701
tBroatch <thomas.broatch98@gmail.com>
2021-03-23 19:09:12 -0700

    Cleaned up predecessor code / tests

--- src/chainReplication/updatePredecessor.go
+++ src/chainReplication/updatePredecessor.go
@@ -14,7 +14,10 @@ type sweeperFunc func(keys util.KeyRange)
 var sweepCache sweeperFunc = kvstore.Sweep
 var transferKeys transferFunc = sendDataTransferReq
 
-// Replace with actual transfer / sweeper functions when merging with shay & brennan code
+/** dummyTransfer and dummySweeper are used for development purposes. They share the
+same parameters as our actual sweeper / transfer functions, so they can be passed
+into our updatePredecessor function to run it without actually modifying the kvStore.
+*/
 func dummyTransfer(destAddr *net.Addr, coordAddr *net.Addr, keys util.KeyRange) {
 	log.Printf("Called Transfer function with range [%v, %v], addr, %v \n", keys.Low, keys.High, (*coordAddr).String())
 }
@@ -23,7 +26,6 @@ func dummySweeper(keys util.KeyRange) {
 	log.Println("Called Sweeper function with range", keys)
 }
 
-// TODO: may need to update both predecessors at once
 func UpdatePredecessors(addr []*net.Addr, keys []uint32) {
 	coarseLock.Lock()
 	defer coarseLock.Unlock()
@@ -45,8 +47,8 @@ func UpdatePredecessors(addr []*net.Addr, keys []uint32) {
 		}
 	}
 	// checkAddresses(addr, keys)
-	checkPredecessors(newPredecessors, transferKeys, sweepCache) // TODO Replace with brennan /shay functions
-	copyPredecessors(newPredecessors)                            // TODO: Not sure if I can do this, seems a bit hacky
+	checkPredecessors(newPredecessors, transferKeys, sweepCache)
+	copyPredecessors(newPredecessors)
 	if newPredecessors[0] != nil {
 		MyKeys.Low = newPredecessors[0].keys.High + 1
 	} else {
@@ -54,12 +56,22 @@ func UpdatePredecessors(addr []*net.Addr, keys []uint32) {
 	}
 }
 
+/**
+Helper function to copy all new nodes to old nodes by value.
+*/
 func copyPredecessors(newPredecessors [3]*predecessorNode) {
 	for i := 0; i < len(newPredecessors); i++ {
 		predecessors[i] = shallowCopy(newPredecessors[i])
 	}
 }
 
+/**
+Helper function used in development. Compares incoming
+addresses / keys to existing ones, then prints the result.
+
+@param addr the new addresses from the membership layer
+@param keys the new keys from the membership layer
+*/
 func checkAddresses(addr []*net.Addr, keys []uint32) {
 	var addrString = ""
 	for i := 0; i < len(addr); i++ {
@@ -90,17 +102,21 @@ func comparePredecessors(newPred *predecessorNode, oldPred *predecessorNode) boo
 	Compares a nodes current chain to its previous one. If there are any changes, will need to send/receive/drop
 	keys.
 
-	This was done through a series of if/else statements.
+	This was done through a series of if/else statements, each handling a different scenario. It was the easiest method
+	for our system with a replication factor of 3 as there not too many scenarios for possible changes to a nodes chain.
+	A different approach would be necessary for a higher replication factor as there would be too many scenarios to
+	cleanly code.
+
 	Scenarios are listed here: https://app.diagrams.net/#G1MaVQbmbZ6cjkAzkG8zbFdaj9r03HWV5A
-	TODO: Currently this code does not handle multiple simultaneous changes,
+	TODO: Currently this code does not handle multiple simultaneous changes within a single heartbeat well,
 		such as a new node joining and another node in the chain failing. The assumption
 		was that this would be a rare enough event.
 	@param newPredecessors are the three nodes previous to the current node in the chain, or
 	nil if there are not enough nodes in the chain
 	@param transferKeys and sweepCache are used to make this function testable - otherwise it would
-	be challenging to keep
+	be challenging to keep. transferKeys initiates a
 
-	Precondition: if newPredAddrX is nil, all nodes newPredAddr(>X) must also be nil. e.g. if newPredAddr1 is nil,
+	Precondition: if newPredAddrX is nil, all nodes newPredAddr(>X) must also be nil. e.g. if newPredecessors[0] is nil,
 	newPredAddr2 and newPredAddr3 must also be nil.
 */
 
@@ -118,11 +134,11 @@ func checkPredecessors(newPredecessors [3]*predecessorNode, transferKeys transfe
 	}
 	PrintKeyChange(newPredecessors)
 
-	log.Println("\n\nNEW PREDECESSOR", pred1Equal, pred2Equal, pred3Equal, "\n")
+	log.Println("\n\nNEW PREDECESSOR", pred1Equal, pred2Equal, pred3Equal)
 
 	// If we newly joined, expect to receive keys
 	if newPred1 != nil && oldPred1 == nil && newPred2 != nil && oldPred2 == nil {
-		log.Println("\n\n EXPECTING TO RECEIVE KEYS FROM PREDECESSORS AFTER BOOTSTRAP\n")
+		log.Print("\n\n EXPECTING TO RECEIVE KEYS FROM PREDECESSORS AFTER BOOTSTRAP\n")
 		addExpectedTransfer(newPred1.addr)
 		addExpectedTransfer(newPred2.addr)
 		return
@@ -134,7 +150,7 @@ func checkPredecessors(newPredecessors [3]*predecessorNode, transferKeys transfe
 		between the second and third predecessor.
 	*/
 	if pred1Equal && pred2Equal {
-		log.Println("\n\n THIRD PREDECESSOR HAS CHANGED\n")
+		log.Print("\n\n THIRD PREDECESSOR HAS CHANGED\n")
 
 		// New node has joined
 		if newPred3 != nil && newPred2 != nil && (oldPred3 == nil || util.BetweenKeys(newPred2.keys.Low, oldPred2.keys.Low, oldPred2.keys.High)) {
@@ -145,7 +161,7 @@ func checkPredecessors(newPredecessors [3]*predecessorNode, transferKeys transfe
 			}
 		}
 	} else if pred1Equal {
-		log.Println("\n\n SECOND PREDECESSOR HAS CHANGED\n")
+		log.Print("\n\n SECOND PREDECESSOR HAS CHANGED\n")
 
 		/*
 			First predecessor is the same, second is different. This could mean
@@ -159,10 +175,10 @@ func checkPredecessors(newPredecessors [3]*predecessorNode, transferKeys transfe
 		if oldPred2 == nil || newPred2 == nil {
 			return
 		} else if comparePredecessors(newPred3, oldPred2) { // New node joined
-			log.Println("\n\nNEW SECOND PREDECESSOR JOINED\n")
+			log.Print("\n\nNEW SECOND PREDECESSOR JOINED\n")
 			sweepCache(oldPred2.keys)
 		} else if comparePredecessors(newPred2, oldPred3) { // P2 Failed. Will be receiving keys from p1 for new p2
-			log.Println("\n\n SECOND PREDECESSOR FAILED\n\n")
+			log.Print("\n\n SECOND PREDECESSOR FAILED\n\n")
 			addExpectedTransfer(newPred2.addr)
 			if oldPred2 != nil && oldPred1 != nil {
 				log.Printf("TRANSFERRING KEYS TO SUCC %v", (*successor.addr).String())
@@ -173,7 +189,7 @@ func checkPredecessors(newPredecessors [3]*predecessorNode, transferKeys transfe
 		}
 
 	} else { // First predecessor node has changed.
-		log.Println("\n\n FIRST PREDECESSOR HAS CHANGED\n")
+		log.Print("\n\n FIRST PREDECESSOR HAS CHANGED\n")
 
 		// If there's no first predecessor, there can only be one node in the system - not enough
 		// for a full chain, so nothing needs to be done in terms of replication.
@@ -181,14 +197,13 @@ func checkPredecessors(newPredecessors [3]*predecessorNode, transferKeys transfe
 			return
 		}
 		if util.BetweenKeys(newPred1.keys.High, oldPred1.keys.High, MyKeys.High) { // New node has joined
-			log.Println("\n\n NEW FIRST PREDECESSOR HAS JOINED\n")
+			log.Print("\n\n NEW FIRST PREDECESSOR HAS JOINED\n")
 
-			// TODO: bootstrap transfer?
 			if oldPred2 != nil {
 				sweepCache(oldPred2.keys)
 			}
 		} else if comparePredecessors(oldPred2, newPred1) { // Node 1 has failed, node 2 is still running
-			log.Println("\n\n FIRST PREDECESSOR FAILED\n")
+			log.Print("\n\n FIRST PREDECESSOR FAILED\n")
 
 			// GOT EXCEPTION HERE
 			if newPred2 != nil {
@@ -202,7 +217,6 @@ func checkPredecessors(newPredecessors [3]*predecessorNode, transferKeys transfe
 				go transferKeys(successor.addr, newPredecessors[0].addr, util.KeyRange{Low: oldPred2.keys.Low, High: oldPred2.keys.High})
 			}
 			addExpectedTransfer(newPred1.addr)
-
 			// TODO: Should also transfer keys between (newPredKey3, oldPredKey2). With
 			// 	our current architecture this is not possible since we do not yet have those keys.
 			//  This should be very rare so may not need to be handled, as the churn is expected to be low.
@@ -220,8 +234,14 @@ func PrintKeyChange(newPredecessors [3]*predecessorNode) {
 	log.Printf("NEW KEYS: %v, %v, %v\n", getPredKey(newPredecessors[0]), getPredKey(newPredecessors[1]), getPredKey(newPredecessors[2]))
 }
 
-// TODO: This will crash the node and log an error message, should ideally never get called. Regardless,
-//  remove this function at the end, only using for debugging to make sure scenarios are properly handled.
+/*
+	Used for debugging purposes. Prints out the changes in the keyrange between new and old
+	predecessors.
+
+	Ideally, this function will never get called - if it does, it means our UpdatePredecessor implementation
+	is not robust enough to handle the different scenarios. While in development, this function would crash
+	the program with log.Fatal to allow us to more easily identify issues.
+*/
 func UnhandledScenarioError(newPredecessors [3]*predecessorNode) {
 	log.Println("ERROR: UNHANDLED SCENARIO: OLD KEYS")
 	for i := 0; i < len(predecessors); i++ {
@@ -239,10 +259,13 @@ func UnhandledScenarioError(newPredecessors [3]*predecessorNode) {
 			log.Println(newPredecessors[i].keys)
 		}
 	}
-	log.Println("UNHANDLED SCENARIO, INTENTIONALLY CRASHING (REMOVE THIS FUNCTION LATER)")
+	log.Println("UNHANDLED SCENARIO")
 
 }
 
+/*
+	Helper function to copy a predecessor node by value.
+*/
 func shallowCopy(orig *predecessorNode) *predecessorNode {
 	if orig == nil {
 		return nil
